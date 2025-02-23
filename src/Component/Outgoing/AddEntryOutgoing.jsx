d2a23f3380694db001e5ab2d47aa46bada20497d
lathiyaharshit <lathiyaharshit19@gmail.com>
2025-02-23 10:29:29 +0530

    remove days

--- src/Component/Outgoing/AddEntryOutgoing.jsx
+++ src/Component/Outgoing/AddEntryOutgoing.jsx
@@ -227,10 +227,10 @@ const AddEntryOutgoing = ({selectedColor}) => {
                             {/* <input type="password" name="repeat_password" id="floating_repeat_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required /> */}
                             {/* <label for="floating_repeat_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Broker Name(દલાલનું નામ)</label> */}
                         {/* </div> */}
-                        <div class="relative z-0 w-full  group">
+                        {/* <div class="relative z-0 w-full  group">
                             <input type="number" name="days" id="days" class="block py-2.5 mb-5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value={formData.days} onChange={handleInputChange} required />
                             <label for="days" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Days</label>
-                        </div>
+                        </div> */}
                     </div>
 
                     <select placeholder="helo" name="brokerName" value={formData._id} onChange={handleInputChange} className='block mb-3 py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'>
